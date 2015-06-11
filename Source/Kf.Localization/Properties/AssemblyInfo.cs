dd88fcc39e9dd7fc507459756e0d70fe80eb501f
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-11 15:21:48 +0200

    Editied AssemblyInfo metadata.

---
+++ Source/Kf.Localization/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Kf.Localization")]
+[assembly: AssemblyDescription("Basic .Net library for supporting resource based localization.")]
+[assembly: AssemblyCompany("KodeFoxx")]
+[assembly: AssemblyProduct("Kf.Localization")]
+[assembly: AssemblyCopyright("Copyright © Yves Schelpe as KodeFoxx 2015")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.*")]
+
