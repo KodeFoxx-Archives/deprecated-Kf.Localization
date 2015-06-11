1b4189ec68b62e822f2c7dead28375af9e47d695
YvesSchelpe_KdG <yves.schelpe@kdg.be>
2015-06-11 15:37:03 +0200

    Added Resx project for specific implementation.

---
+++ Source/Technologies/Resx/Kf.Localization.Resx/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Kf.Localization.Resx")]
+[assembly: AssemblyDescription("Resx implementation for the Kf.Localization.")]
+[assembly: AssemblyCompany("KodeFoxx")]
+[assembly: AssemblyProduct("Kf.Localization.Resx")]
+[assembly: AssemblyCopyright("Copyright © Yves Schelpe as KodeFoxx 2015")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("1.0.*")]
